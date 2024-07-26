610790f2297082211015623eca92c4e54932ad49
Rajsinghrajputana <154723115+Rajsinghrajputana@users.noreply.github.com>
2024-07-26 13:11:56 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,7 +2,21 @@ const decodeTheRing = function (s, p) {
 
     // write your code here
 
-
+    if (s.length !== p.length) {
+      return false;
+    }
+  
+    for (let i = 0; i < s.length; i++) {
+      if (p[i] === "*") {
+        continue;
+      }
+  
+      if (s[i] !== p[i]) {
+        return false;
+      }
+    }
+    // And last mei sare match ho jye to
+    return true;
   };
   
   module.exports = decodeTheRing;
