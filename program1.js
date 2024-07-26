917ae9f560bf5879e728fc2c4039e07f31461d54
Rajsinghrajputana <154723115+Rajsinghrajputana@users.noreply.github.com>
2024-07-26 12:38:11 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,7 +2,30 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
-  
+  let islands = 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  function dfs(i, j) {
+    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === "W") {
+      return;
+    }
+
+    grid[i][j] = "W";
+
+    return dfs(i + 1, j) || dfs(i - 1, j) || dfs(i, j + 1) || dfs(i, j - 1);
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === "L") {
+        islands += 1;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return islands;
 
 };
 
